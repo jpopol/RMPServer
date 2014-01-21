5585d830e07f7224c32411830acb8a17a0470581
jpopol <bonnet.jp@gmail.com>
2014-01-21 22:02:03 +0000

    * Fixing picture download bug for id 140   Making pic download Async using Akka

--- app/controllers/Portrait.scala
+++ app/controllers/Portrait.scala
@@ -1,12 +1,19 @@
 package controllers
 
+import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc.{Controller, Action}
+import play.api.libs.concurrent.Akka
+import play.api.Play.current
+import play.Logger
 
 object Portrait extends Controller {
 
   def portrait(id: Long) = Action {
     val MimeType = "image/jpg"
-     Ok( download(id)).as(MimeType)
+    val portraitPromise = Akka.future(download(id))
+    Async {
+      portraitPromise.map(p => Ok(p).as(MimeType))
+    }
   }
 
   def download(id: Long) = {
@@ -15,20 +22,25 @@ object Portrait extends Controller {
 
     var in: InputStream = null
     var byteArray:Array[Byte] = null
-    try {
-      val url = new URL(f"http://www.kildarestreet.com/images/mpsL/$id%s.jpg")
 
-      val connection = url.openConnection().asInstanceOf[HttpURLConnection]
-      connection.setRequestMethod("GET")
-      in = connection.getInputStream
-      byteArray = Stream.continually(in.read).takeWhile(-1 !=).map(_.toByte).toArray
+    def fetch(id: Long, pictureType:String) = Option[Array[Byte]]{
+      try {
+        val url = new URL(f"http://www.kildarestreet.com/images/mpsL/$id%s.$pictureType%s")
 
-    } catch {
-      case e: Exception => println(e.printStackTrace())
-    } finally {
-      in.close
+        val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+        connection.setRequestMethod("GET")
+        in = connection.getInputStream
+        byteArray = Stream.continually(in.read).takeWhile(-1 !=).map(_.toByte).toArray
+
+      } catch {
+        case e: Exception => Logger.error(f"Can't find picture as $pictureType for id: $id", e)
+      } finally {
+        if (in != null) in.close
+      }
+
+      byteArray
     }
 
-    byteArray
+    fetch(id,"jpg").getOrElse(fetch(id,"png").get)
   }
 }
