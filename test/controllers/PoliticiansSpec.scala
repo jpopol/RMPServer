dedb1abafdf1604c46e92bc4ff53202d47404fc1
jpopol <bonnet.jp@gmail.com>
2014-02-16 18:43:17 +0000

    * Modified integration test to be positive/negative

--- test/controllers/PoliticiansSpec.scala
+++ test/controllers/PoliticiansSpec.scala
@@ -29,7 +29,7 @@ class PoliticiansSpec extends Specification {
         status(result) must equalTo(OK)
         contentType(result) must beSome("text/html")
         contentAsString(result) must contain("Independent")
-        contentAsString(result) must not contain("Sinn Fein") and not contain("Labour")
+        contentAsString(result) must not contain("Sinn Fein") and not contain("Labour") and contain("Independent")
 
       }
     }
